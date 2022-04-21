2982cdbd61b8dd72d3ea94504fef12e2f14382ed
leafarpontes <rafapontes2008@hotmail.com>
2022-04-21 09:34:21 -0300

    Added connection to MongoDB using dotenv

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-// configuração inicial
+// initial configuration
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+require('dotenv').config()
 
-// forma de ler JSON / middlewares
+// reading JSON / middlewares
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-// rota inicial / endpoint
+// initial route / endpoint
 app.get('/', (req, res) => {
-  res.json({message: "Oi Express!"});
+  res.json({message: "Hi Express!"});
 });
 
-// entregar uma porta
-app.listen(3000);
+// mongoose connection
+mongoose
+  .connect(`${process.env.MONGO_DB_CONNECTION}`)
+  .then(() => {
+    console.log("Connected to MongoDB.")
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
